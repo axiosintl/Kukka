51e7f07b12e84f076b84ed15c029c8d5b6aa2978
Gangadhar Vaddepalli <gvaddepalli@axiosonline.com>
2023-07-25 13:56:13 -0400

    Add files via upload

---
+++ sendmailwithattachment/src/test/java/com/example/sendmailwithattachment/SendmailwithattachmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sendmailwithattachment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendmailwithattachmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
